852eaf565fa3a3a27e4f3d3f3a64c36912e2a394
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-10-13 13:18:31 +0100

    seventh commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const navigation = document.querySelector(".nav__lists");
 const burger = document.querySelector(".nav__burger");
+const links = document.querySelectorAll("a:link");
 
 burger.addEventListener("click", () => {
   navigation.classList.toggle("nav--open");
@@ -9,4 +10,24 @@ burger.addEventListener("click", () => {
 navigation.addEventListener("click", () => {
   navigation.classList.remove("nav--open");
   burger.classList.remove("nav__burger--open");
+});
+
+links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 });
