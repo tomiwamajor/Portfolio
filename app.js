b05b5b3697de3c157f8fc025275abe9700fe2c8d
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-10-23 17:00:26 +0100

    'changes'

--- app.js
+++ app.js
@@ -17,23 +17,3 @@ developer.addEventListener("click", () => {
   navigation.classList.remove("nav--open");
   burger.classList.remove("nav__burger--open");
 });
-
-// links.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
-
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-
-//     // Scroll to other links
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
-//   });
-// });
